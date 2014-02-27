38e9089f4e5a09aedb631965c9a7e220add9475b
Adam Taylor <hvfcrazy@hotmail.com>
2014-02-27 11:59:10 +0000

    Added scores array (to be replaced by MySQL query)

--- whenPlanesCollide/connection.php
+++ whenPlanesCollide/connection.php
@@ -1,4 +1,11 @@
 <?php
 
-echo "Hello, this is server";
+scores = array(
+    AdamT  => 9001,
+    Bob => 10,
+    Jim => value3,
+)
+foreach ($scores as $key => $value) {
+    echo "$key; $value";
+}
 ?>
