c73d3b2f3d3b2a1a03dc4233d6980b54362aff93
adam <hvfcrazy@hotmail.com>
2014-02-27 18:12:30 +0000

    Change mysql order to ASC so the hashmap swaps it round to be the correct way round

--- whenPlanesCollide/connection.php
+++ whenPlanesCollide/connection.php
@@ -5,7 +5,7 @@ mysql_connect("localhost", "www-data", "www-datapw") or die(mysql_error());
 mysql_select_db("teamwaw") or die(mysql_error());
 
 // Get all the data from the "example" table
-$result = mysql_query("SELECT * FROM  `HighScores` ORDER BY  `HighScores`.`Score` DESC LIMIT 10") or die(mysql_error());  
+$result = mysql_query("SELECT * FROM  `HighScores` ORDER BY  `HighScores`.`Score` ASC LIMIT 10") or die(mysql_error());  
 
 // keeps getting the next row until there are no more to get
 while($row = mysql_fetch_array( $result )) {
