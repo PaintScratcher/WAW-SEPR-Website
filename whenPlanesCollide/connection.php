d5526f179e45c22d42c8309e4f668833755fffe1
adam <hvfcrazy@hotmail.com>
2014-02-27 18:10:29 +0000

    Added mysql query

--- whenPlanesCollide/connection.php
+++ whenPlanesCollide/connection.php
@@ -1,11 +1,15 @@
 <?php
 
-$scores = array(
-    AdamT  => 9001,
-    Bob => 10,
-    Jim => 90,
-);
-foreach ($scores as $key => $value) {
-    echo $key.":".$value."\n";
-}
+// Make a MySQL Connection
+mysql_connect("localhost", "www-data", "www-datapw") or die(mysql_error());
+mysql_select_db("teamwaw") or die(mysql_error());
+
+// Get all the data from the "example" table
+$result = mysql_query("SELECT * FROM  `HighScores` ORDER BY  `HighScores`.`Score` DESC LIMIT 10") or die(mysql_error());  
+
+// keeps getting the next row until there are no more to get
+while($row = mysql_fetch_array( $result )) {
+	// Print out the contents of each row into a table
+	echo $row['Name'].":".$row['Score']."\n";
+} 
 ?>
